55d008e6e319489ce67382416f56f09f8c7479eb
Sai venkat <36558513+saivenkat288@users.noreply.github.com>
2021-11-01 10:06:55 +0530

    Add files via upload

--- config/config_reader.py
+++ config/config_reader.py
@@ -5,8 +5,8 @@ from functools import lru_cache
 load_dotenv(verbose=True)
 
 class Settings(BaseSettings):
-    db_password: str = "rootbi2i"
-    db_username: str = "postgres"
+    db_password: str = "cbc456aacc164bb4c3fa7a1c68b0df23c1b42ab818f8ee6fe588bd9d8559b4fd"
+    db_username: str = "bdeeqglxayvwpz"
     class Config:
         env_prefix = "" 
         env_file = "../.env"
